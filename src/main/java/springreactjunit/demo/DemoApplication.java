8d723cb6c6475648c99e625ad91b01e71edfe39a
jjosephgalicio <jjosephgalicio@gmail.com>
2022-06-16 08:48:00 +0800

    Test Commit

---
+++ src/main/java/springreactjunit/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package springreactjunit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
